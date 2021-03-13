a2b956ef4a74bce3020f93924e188f849f2fb2ae
vagrant <vagrant@localhost.localdomain>
2021-03-13 10:10:23 +0000

    ticket登録、ticket購入機能の追加

--- app/controllers/users/purchases_controller.rb
+++ app/controllers/users/purchases_controller.rb
@@ -1,2 +1,35 @@
 class Users::PurchasesController < ApplicationController
+
+	before_action :authenticate_user!
+	before_action :is_banned, only: :create
+
+	def new
+		@tickets = Ticket.all
+		@purchase = Purchase.new
+		@purchases = current_user.purchases.page(params[:page]).per(5)
+	end
+
+	def create
+		if params[:purchase][:ticket_id] == "" || params[:purchase][:quantity] == ""
+			redirect_to new_users_purchase_path, flash: { error: "購入内容を入力してください。" }
+		else
+			@user = current_user
+			@purchase = @user.purchases.new(purchase_params)
+			@ticket = Ticket.find_by(id: params[:purchase][:ticket_id])
+			@purchase.charge = @ticket.price * params[:purchase][:quantity].to_i
+			@user.update(ticket: @user.ticket + @ticket.quantity * params[:purchase][:quantity].to_i)
+			if @purchase.save
+				redirect_to users_user_path(current_user), flash: { notice: "チケットが#{@ticket.quantity * params[:purchase][:quantity].to_i}枚追加されました。" }
+			else
+				render :new
+			end
+		end
+	end
+
+	private
+
+	def purchase_params
+		params.require(:purchase).permit(:ticket_id, :payment_method, :quantity)
+	end
+
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,9 @@
 class Ticket < ApplicationRecord
+
+	has_many :purchases
+
+	validates :name, presence: true
+	validates :price, presence: true
+	validates :quantity, presence: true
+
 end
